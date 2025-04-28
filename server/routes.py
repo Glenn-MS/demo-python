bf8ce5d88234a2d1958a408413e4c1afd9cfc553
Glenn Rosedom <13999846+Glenn-MS@users.noreply.github.com>
2025-04-28 15:00:19 +0200

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -28,14 +28,4 @@ def index():
         cursor.execute("SELECT name, author, read FROM books")
         books = [Book(*row) for row in cursor]
 
-
-    # Unsafe usage of yaml.load()
-    # This would delete all files "!!python/object/apply:os.system ["rm -rf /"]"
-    # Using "ls" instead
-
-    user_input = """
-    !!python/object/apply:os.system ["ls"]
-    """
-    data = yaml.load(user_input)  # This can execute the malicious command
-
     return render_template('books.html', books=books)
