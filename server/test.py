c2aca36d3e2e955bc1507ebc8dcf05bcb02bf4b2
Glenn Rosedom <13999846+Glenn-MS@users.noreply.github.com>
2025-04-28 15:11:46 +0200

    Update test.py

--- server/test.py
+++ server/test.py
@@ -1,10 +1,14 @@
 import yaml
+import hashlib
 
 # Unsafe usage of yaml.load()
 # This would delete all files "!!python/object/apply:os.system ["rm -rf /"]"
 # Using "ls" instead.
-
 user_input = """
 !!python/object/apply:os.system ["ls"]
 """
 data = yaml.load(user_input)  # This can execute the malicious command
+
+# Unsafe usage of hashlib.md5() for password hashing
+password = "password123"
+hashed_password = hashlib.md5(password.encode()).hexdigest()  # This is insecure
